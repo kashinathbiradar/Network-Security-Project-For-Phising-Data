b21a733b66de418300373cb98bd21e963682b958
Kashinath_Biradar <108365128+kashinathbiradar@users.noreply.github.com>
2025-09-14 23:13:06 +0530

    Update test_mongodb.py

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://krishaitechnologies:<@password>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://krishaitechnologies:<@kashi7585>@cluster0.wnqb6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
@@ -11,4 +11,5 @@ try:
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
+
     print(e)
